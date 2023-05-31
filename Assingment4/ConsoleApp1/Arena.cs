846e76fa0960e8c41c04c6843f0497646c599c1a
CedricPedric <cedricnelson03@gmail.com>
2023-05-31 14:01:21 +0200

    assingment 4 is af en ga beginnen aan 5

--- Assingment4/ConsoleApp1/Arena.cs
+++ Assingment4/ConsoleApp1/Arena.cs
@@ -8,5 +8,86 @@ namespace ConsoleApp1
 {
     public class Arena
     {
+        public int rounds = 0;
+        public int battles = 0;
+
+        public Trainer trainer1;
+        public Trainer trainer2;
+        private int trainer1CurrentPokemon = 0;
+        private int trainer2CurrentPokemon = 0;
+        public string whoWon = "";
+
+        public Arena(Trainer trainer1, Trainer trainer2)
+        {
+            this.trainer1 = trainer1;
+            this.trainer2 = trainer2;
+        }
+
+        public void Stuff()
+        
+        {
+            //This randomizes the pokemon!
+
+            Random rand = new Random();
+            trainer1.belt = trainer1.belt.OrderBy(_ => rand.Next()).ToList();
+            trainer2.belt = trainer2.belt.OrderBy(_ => rand.Next()).ToList();
+
+            Console.WriteLine("Trainer 1's Pokemon in Order");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(trainer1.belt[i].pokemon.name);
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine("Trainer 2's Pokemon in Order");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(trainer2.belt[i].pokemon.name);
+            }
+            Console.WriteLine("---------------");
+            while (true)
+            {
+
+                if (trainer1.belt.Count == trainer1CurrentPokemon && trainer2.belt.Count == trainer1CurrentPokemon)
+                {
+                    Console.WriteLine("Its a Draw!");
+                    battles++;
+                    break;
+                }
+                else if (trainer1.belt.Count == trainer1CurrentPokemon )
+                {
+                    Console.WriteLine("Trainer2 Won!");
+                    battles++;
+                    break;
+                }
+                else if (trainer2.belt.Count == trainer2CurrentPokemon )
+                {
+                    Console.WriteLine("Trainer1 Won!");
+                    battles++;
+                    break;
+                }
+                Console.WriteLine(trainer1.belt[trainer1CurrentPokemon].pokemon.name);
+                Console.WriteLine(trainer2.belt[trainer2CurrentPokemon].pokemon.name);
+
+                Battle battle = new Battle();
+                whoWon = battle.BattleBitch(trainer1.belt[trainer1CurrentPokemon].pokemon, trainer2.belt[trainer2CurrentPokemon].pokemon);
+                if (whoWon == "Draw")
+                {
+                    trainer1CurrentPokemon++;
+                    trainer2CurrentPokemon++;
+                }
+                else if (whoWon == "Trainer1")
+                {
+                    trainer2CurrentPokemon++;
+                }
+                else if (whoWon == "Trainer2")
+                {
+                    trainer1CurrentPokemon++;
+                }
+                rounds++;
+            }
+            Console.WriteLine("SCORES:");
+            Console.WriteLine("rounds: " + rounds);
+            Console.WriteLine("battles: " + battles);
+        }
     }
 }
--- Assingment4/ConsoleApp1/Program.cs
+++ Assingment4/ConsoleApp1/Program.cs
@@ -21,30 +21,18 @@ class Program
         Console.WriteLine(trainer2.name);
 
         //Code to fill the belts with pokeballs
-       
-        trainer1.belt.Add(new Pokeball(new Bulbasaur("Bulbasaur 1")));
+
         trainer1.belt.Add(new Pokeball(new Charmander("Charmander 1")));
+        trainer1.belt.Add(new Pokeball(new Bulbasaur("Bulbasaur 1")));
         trainer1.belt.Add(new Pokeball(new Squirtle("Squirtle 1")));
 
-        trainer2.belt.Add(new Pokeball(new Bulbasaur("Bulbasaur 2")));
-        trainer2.belt.Add(new Pokeball(new Charmander("Charmander 2")));
         trainer2.belt.Add(new Pokeball(new Squirtle("Squirtle 2")));
+        trainer2.belt.Add(new Pokeball(new Charmander("Charmander 2")));
+        trainer2.belt.Add(new Pokeball(new Bulbasaur("Bulbasaur 2")));
 
-            //The "Battle"
-            for (int i = 0; i < 3; i++)
-        {
-            Console.WriteLine(trainer1.name + " send out " + trainer1.belt[i].pokemon.name + "!");
-            Console.WriteLine("It cries out its name!");
-            trainer1.belt[i].pokemon.BattleCry();
-            Console.WriteLine(trainer1.name + " returned " + trainer1.belt[i].pokemon.name);
-            Thread.Sleep(1000); //will sleep for 1 sec
-
-            Console.WriteLine(trainer2.name + " send out " + trainer2.belt[i].pokemon.name + "!");
-            Console.WriteLine("It cries out its name!");
-            trainer2.belt[i].pokemon.BattleCry();
-            Console.WriteLine(trainer2.name + " returned " + trainer2.belt[i].pokemon.name);
-            Thread.Sleep(1000); //will sleep for 1 sec
-        }
+        //The "Battle"
+        Arena arena = new Arena(trainer1, trainer2);
+        arena.Stuff();
         //The End
         Console.WriteLine("The Game Has Ended! Do you want to see it again? (Yes or No):");
         userInput = Console.ReadLine();
